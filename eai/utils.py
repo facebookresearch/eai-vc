5b2f1d358c8541f688935936af02b9612244f0b4
Arjun Majumdar <arjun.majum@gmail.com>
2022-03-22 14:39:19 -0400

    Add ViT

--- eai/utils.py
+++ eai/utils.py
@@ -2,9 +2,27 @@ import os
 
 import torch
 
+from eai.models.resnet_gn import ResNet
+from eai.models.vit import VisionTransformer
+from timm.models.vision_transformer import resize_pos_embed
 
-def load_encoder(encoder, path, state_dict_key="teacher"):
+
+def load_encoder(encoder, path):
     assert os.path.exists(path)
-    state_dict = torch.load(path, map_location="cpu")[state_dict_key]
-    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-    return encoder.load_state_dict(state_dict=state_dict, strict=False)
+    if isinstance(encoder.backbone, ResNet):
+        state_dict = torch.load(path, map_location="cpu")["teacher"]
+        state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+        return encoder.load_state_dict(state_dict=state_dict, strict=False)
+    elif isinstance(encoder.backbone, VisionTransformer):
+        model = encoder.backbone
+        state_dict = torch.load(path, map_location="cpu")["model"]
+        if state_dict["pos_embed"].shape != model.pos_embed.shape:
+            state_dict["pos_embed"] = resize_pos_embed(
+                state_dict["pos_embed"],
+                model.pos_embed,
+                getattr(model, "num_tokens", 1),
+                model.patch_embed.grid_size,
+            )
+        return model.load_state_dict(state_dict=state_dict, strict=False)
+    else:
+        raise ValueError(f"unknown encoder backbone")
--- eai/visual_encoder.py
+++ eai/visual_encoder.py
@@ -4,6 +4,7 @@ from habitat_baselines.rl.ddppo.policy.running_mean_and_var import RunningMeanAn
 from torch import nn as nn
 
 from eai.models import resnet_gn as resnet
+from eai.models import vit
 
 
 class VisualEncoder(nn.Module):
@@ -23,32 +24,42 @@ class VisualEncoder(nn.Module):
         else:
             self.running_mean_and_var = nn.Sequential()
 
-        make_backbone = getattr(resnet, backbone)
-        self.backbone = make_backbone(input_channels, baseplanes, ngroups)
+        if "resnet" in backbone:
+            make_backbone = getattr(resnet, backbone)
+            self.backbone = make_backbone(input_channels, baseplanes, ngroups)
 
-        final_spatial = int(spatial_size * self.backbone.final_spatial_compress)
-        after_compression_flat_size = 2048
-        num_compression_channels = int(
-            round(after_compression_flat_size / (final_spatial**2))
-        )
-        self.compression = nn.Sequential(
-            nn.Conv2d(
-                self.backbone.final_channels,
+            final_spatial = int(spatial_size * self.backbone.final_spatial_compress)
+            after_compression_flat_size = 2048
+            num_compression_channels = int(
+                round(after_compression_flat_size / (final_spatial**2))
+            )
+            self.compression = nn.Sequential(
+                nn.Conv2d(
+                    self.backbone.final_channels,
+                    num_compression_channels,
+                    kernel_size=3,
+                    padding=1,
+                    bias=False,
+                ),
+                nn.GroupNorm(1, num_compression_channels),
+                nn.ReLU(True),
+            )
+
+            output_shape = (
                 num_compression_channels,
-                kernel_size=3,
-                padding=1,
-                bias=False,
-            ),
-            nn.GroupNorm(1, num_compression_channels),
-            nn.ReLU(True),
-        )
-
-        self.output_shape = (
-            num_compression_channels,
-            final_spatial,
-            final_spatial,
-        )
-        self.output_size = np.prod(self.output_shape)
+                final_spatial,
+                final_spatial,
+            )
+            self.output_size = np.prod(output_shape)
+        elif "vit" in backbone:
+            make_backbone = getattr(vit, backbone)
+            self.backbone = make_backbone(
+                img_size=spatial_size, use_head=False, global_pool=True
+            )
+            self.compression = nn.Identity()
+            self.output_size = self.backbone.embed_dim
+        else:
+            raise ValueError("unknown backbone {}".format(backbone))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # type: ignore
         x = self.running_mean_and_var(x)
