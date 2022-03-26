c03a20b8e4474ab644d310fe3cf0110f87b4568f
Arjun Majumdar <arjun.majum@gmail.com>
2022-03-26 17:43:02 -0400

    Fix data loading

--- tmae/util/path_dataset.py
+++ tmae/util/path_dataset.py
@@ -33,17 +33,27 @@ class PathDataset(VisionDataset):
         folder = self.folders[idx]
         images = self.files[folder]
         offset = np.random.randint(0, self.max_offset)
-        img_idx = np.random.randint(0, len(images) - offset)
-        img1 = self._get_image(folder, img_idx)
-        img2 = self._get_image(folder, img_idx + offset)
-        return img1, img2, offset
+        img1_idx = np.random.randint(0, len(images) - offset)
+        img2_idx = min(img1_idx + offset, len(images) - 1)
+        img1 = self._get_image(folder, img1_idx)
+        img2 = self._get_image(folder, img2_idx)
+        return img1, img2, img2_idx - img1_idx
 
 
 if __name__ == "__main__":
     from torch.utils.data import DataLoader
 
-    dataset = PathDataset(root="data/path-dataset/train")
+    dataset = PathDataset(root="data/datasets/hm3d+gibson/v1/train")
     loader = DataLoader(dataset, batch_size=2, shuffle=True)
     for imgs1, imgs2, offsets in loader:
-        print(imgs1.shape, imgs2.shape, offsets.shape)
+        print(imgs1.shape, imgs2.shape, offsets)
         break
+    print()
+
+    # print dataset info
+    lengths = [len(item) for _, item in dataset.files.items()]
+    print("num paths:  {:,}".format(len(lengths)))
+    print("num images: {:,}".format(sum(lengths)))
+    print("avg length: {:.1f}".format(sum(lengths) / len(lengths)))
+    print("lengths:    [{}:{}]".format(min(lengths), max(lengths)))
+    print()
