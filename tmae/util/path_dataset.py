bd454a6d7eac939b373d493e926928689a4c8a54
Arjun Majumdar <arjun.majum@gmail.com>
2022-03-27 14:32:30 -0400

    Fix dataset

--- tmae/util/path_dataset.py
+++ tmae/util/path_dataset.py
@@ -13,24 +13,27 @@ class PathDataset(VisionDataset):
     ):
         super().__init__(root=root, transform=transform)
         self.folders = sorted(glob.glob(os.path.join(self.root, "*", "*")))
-        self.files = {}
-        for folder in self.folders:
+        self.files, self.idx_to_folder, file_idx = {}, {}, 0
+        for folder_idx, folder in enumerate(self.folders):
             self.files[folder] = sorted(glob.glob(os.path.join(folder, "*.jpg")))
+            for idx in range(len(self.files[folder])):
+                self.idx_to_folder[file_idx + idx] = folder_idx
+            file_idx += len(self.files[folder])
         self.transform = transform
         self.max_offset = max_offset
 
     def _get_image(self, folder, idx):
         path = self.files[folder][idx]
-        img = np.array(Image.open(path).convert("RGB"))
+        img = Image.open(path).convert("RGB")
         if self.transform is not None:
             img = self.transform(img)
         return img
 
     def __len__(self) -> int:
-        return len(self.folders)
+        return len(self.idx_to_folder)
 
     def __getitem__(self, idx) -> Tuple[np.ndarray, np.ndarray]:
-        folder = self.folders[idx]
+        folder = self.folders[self.idx_to_folder[idx]]
         images = self.files[folder]
         offset = np.random.randint(0, self.max_offset)
         img1_idx = np.random.randint(0, len(images) - offset)
@@ -42,8 +45,11 @@ class PathDataset(VisionDataset):
 
 if __name__ == "__main__":
     from torch.utils.data import DataLoader
+    from torchvision.transforms import ToTensor
 
-    dataset = PathDataset(root="data/datasets/hm3d+gibson/v1/train")
+    dataset = PathDataset(
+        root="data/datasets/hm3d+gibson/v1/train", transform=ToTensor()
+    )
     loader = DataLoader(dataset, batch_size=2, shuffle=True)
     for imgs1, imgs2, offsets in loader:
         print(imgs1.shape, imgs2.shape, offsets)
@@ -52,6 +58,7 @@ if __name__ == "__main__":
 
     # print dataset info
     lengths = [len(item) for _, item in dataset.files.items()]
+    print(dataset)
     print("num paths:  {:,}".format(len(lengths)))
     print("num images: {:,}".format(sum(lengths)))
     print("avg length: {:.1f}".format(sum(lengths) / len(lengths)))
