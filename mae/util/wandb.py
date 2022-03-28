d745bac577a242b0f5908094895e12c54e4cec18
Arjun Majumdar <arjun.majum@gmail.com>
2022-03-28 16:01:31 -0400

    Add wandb utils

---
+++ mae/util/wandb.py
@@ -0,0 +1,55 @@
+import os
+from argparse import ArgumentParser, Namespace
+
+import wandb
+
+
+def setup_wandb_parser(parser: ArgumentParser):
+    parser.add_argument(
+        "--wandb_name",
+        default="",
+        type=str,
+        help="name to be used for wandb logging",
+    )
+    parser.add_argument(
+        "--wandb_mode",
+        default="online",
+        type=str,
+        help="wandb mode to use for storing data,"
+        "choose online, offline and disabled",
+    )
+
+
+def setup_wandb_args(args: Namespace):
+    if args.output_dir:
+        args.output_dir = os.path.join(args.output_dir, args.wandb_name)
+
+
+def setup_wandb(args, project="mae_training"):
+    os.makedirs(args.output_dir, exist_ok=True)
+    wandb_filename = os.path.join(args.output_dir, "wandb_id.txt")
+    # if file exists, then we are resuming from a previous eval
+    if os.path.exists(wandb_filename):
+        with open(wandb_filename, "r") as file:
+            wandb_id = file.read().rstrip("\n")
+
+        wandb.init(
+            id=wandb_id,
+            project=project,
+            config=args,
+            mode=args.wandb_mode,
+            resume="must",
+        )
+    else:
+        wandb_id = wandb.util.generate_id()
+        with open(wandb_filename, "w") as file:
+            file.write(wandb_id)
+        wandb.init(
+            id=wandb_id,
+            project=project,
+            config=args,
+            mode=args.wandb_mode,
+        )
+
+    wandb.run.name = args.wandb_name
+    wandb.run.save()
