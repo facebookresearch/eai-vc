93fbfc33a6a4b331301f4d86189174dd1097821c
Arjun Majumdar <arjun.majum@gmail.com>
2022-03-29 09:05:53 -0400

    Add debug script

---
+++ mae/debug.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NGPUS=$(nvidia-smi --list-gpus | wc -l)
+
+DATA="/srv/flash1/amajumdar36/image-datasets/imagenet/train"
+
+set -x
+torchrun \
+--nproc_per_node $NGPUS \
+main_pretrain.py \
+--batch_size 128 \
+--epochs 800 \
+--accum_iter 4 \
+--model mae_vit_base_patch16 \
+--mask_ratio 0.75 \
+--norm_pix_loss \
+--weight_decay 0.05 \
+--blr 1.5e-4 \
+--warmup_epochs 40 \
+--data_path $DATA \
+--wandb_name "debug" \
+--wandb_mode "online" \
