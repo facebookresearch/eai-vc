Fix masking

eai/models/vit.py
@@ -57,25 +57,31 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
         B = x.shape[0]
         x = self.patch_embed(x)
 
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-        x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed
-        x = self.pos_drop(x)
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
 
+        # masking: length -> length * mask_ratio
         if self.mask_ratio is not None:
             x, _, _ = self.random_masking(x, mask_ratio=self.mask_ratio)
 
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
         for blk in self.blocks:
             x = blk(x)
-
         if not self.use_fc_norm:
             x = self.norm(x)
 
+        # global pooling or cls token
         if self.global_pool:
             x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
         else:
             x = x[:, 0]  # cls token
 
+        # use fc_norm layer
         if self.use_fc_norm:
             x = self.fc_norm(x)
 