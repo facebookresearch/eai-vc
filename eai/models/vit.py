35ee15c6850c3abce4ffe8be06d35a1e7d632f13
Arjun Majumdar <arjun.majum@gmail.com>
2022-03-31 21:03:33 -0400

    Remove fc_norm

--- eai/models/vit.py
+++ eai/models/vit.py
@@ -19,10 +19,6 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
 
         self.global_pool = global_pool
         if self.global_pool:
-            norm_layer = kwargs['norm_layer']
-            embed_dim = kwargs['embed_dim']
-            self.fc_norm = norm_layer(embed_dim)
-
             del self.norm  # remove the original norm
 
         self.mask_ratio = mask_ratio
@@ -74,7 +70,7 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
 
         if self.global_pool:
             x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
-            outcome = self.fc_norm(x)
+            outcome = x
         else:
             x = self.norm(x)
             outcome = x[:, 0]
