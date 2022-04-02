a49acdc50fc8b6f2042baf581280931b4c2e1b4c
Arjun Majumdar <arjun.majum@gmail.com>
2022-04-02 09:49:44 -0400

    Update 00-tmae-hm3d+gibson.sh

--- scripts/tmae/00-tmae-hm3d+gibson.sh
+++ scripts/tmae/00-tmae-hm3d+gibson.sh
@@ -3,23 +3,26 @@
 #SBATCH --output log.out
 #SBATCH --error log.err
 #SBATCH --gres gpu:8
-#SBATCH --nodes 2
+#SBATCH --nodes 1
 #SBATCH --cpus-per-task 7
 #SBATCH --ntasks-per-node 8
 #SBATCH --partition short
 #SBATCH --constraint a40
 
-DATA="data/datasets/hm3d+gibson/v1/train"
+MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
+export MASTER_ADDR
 
 cd tmae
 source activate eai
+
+DATA="data/datasets/hm3d+gibson/v1/train"
 
 set -x
 srun python -u \
 main_pretrain.py \
 --batch_size 128 \
 --epochs 800 \
---accum_iter 2 \
+--accum_iter 4 \
 --model mae_vit_base_patch16 \
 --max_offset 16 \
 --mask_ratio1 0.75 \
