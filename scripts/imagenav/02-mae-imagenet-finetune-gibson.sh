b4191cc608a6c30a5a63759aeaca9a757f0d4bd7
Arjun Majumdar <arjun.majum@gmail.com>
2022-04-04 22:46:22 -0400

    Update finetune script

--- scripts/imagenav/02-mae-imagenet-finetune-gibson.sh
+++ scripts/imagenav/02-mae-imagenet-finetune-gibson.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name mae
+#SBATCH --job-name imagenav
 #SBATCH --output log.out
 #SBATCH --error log.err
-#SBATCH --gres gpu:8
+#SBATCH --gres gpu:4
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 7
-#SBATCH --ntasks-per-node 8
+#SBATCH --ntasks-per-node 4
 #SBATCH --signal USR1@600
 #SBATCH --partition short
 #SBATCH --constraint a40
+#SBATCH --requeue
 
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
@@ -24,5 +25,8 @@ python -u run.py \
 --exp-config configs/experiments/imagenav.yaml \
 --run-type train \
 RL.POLICY.backbone vit_base_patch16 \
-RL.POLICY.pretrained_encoder data/models/mae_pretrain_vit_base_full.pth \
+RL.POLICY.vit_use_fc_norm False \
+RL.POLICY.vit_global_pool False \
+RL.POLICY.vit_mask_ratio None \
+RL.POLICY.pretrained_encoder data/models/osd_1_45m_mae_base_01.pth \
 RL.POLICY.freeze_backbone False \
