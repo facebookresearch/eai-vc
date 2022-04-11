4ff9a9b37b5a4c79655d80b27706693088b7be87
Arjun Majumdar <arjun.majum@gmail.com>
2022-04-11 15:30:28 -0400

    Add debug script

---
+++ downstream/debug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NGPUS=$(nvidia-smi --list-gpus | wc -l)
+
+DATA="data/datasets/imagenet"
+CKPT="../data/models/mae_pretrain_vit_base_full.pth"
+
+set -x
+torchrun \
+--nproc_per_node $NGPUS \
+main_linprobe.py \
+--batch_size 512 \
+--model vit_base_patch16 \
+--cls_token \
+--finetune ${CKPT} \
+--epochs 90 \
+--blr 0.1 \
+--weight_decay 0.0 \
+--dist_eval \
+--num_workers 6 \
+--data_path $DATA \
