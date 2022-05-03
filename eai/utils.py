ad2c8626e6610777ed1d14e99e16df5b1ab7023b
Karmesh Yadav <karmeshyadav@fb.com>
2022-05-02 19:01:54 -0700

    show metrics on videos

--- eai/utils.py
+++ eai/utils.py
@@ -7,12 +7,19 @@ from typing import (
     Union,
 )
 
+import cv2
 import numpy as np
 import torch
 from timm.models.vision_transformer import resize_pos_embed
 import wandb
 
-from habitat.utils.visualizations.utils import images_to_video
+from habitat.utils.visualizations.utils import (
+    images_to_video,
+    append_text_to_image,
+    draw_collision,
+    tile_images,
+)
+from habitat.utils.visualizations import maps
 
 from eai.models.resnet_gn import ResNet
 from eai.models.vit import VisionTransformer
@@ -122,6 +129,67 @@ def poll_checkpoint_folder(
 
     return None, previous_ckpt_ind
 
+def observations_to_image(observation: Dict, info: Dict) -> np.ndarray:
+    r"""Generate image of single frame from observation and info
+    returned from a single environment step().
+
+    Args:
+        observation: observation returned from an environment step().
+        info: info returned from an environment step().
+
+    Returns:
+        generated image of a single frame.
+    """
+    render_obs_images: List[np.ndarray] = []
+    for sensor_name in observation:
+        if "rgb" in sensor_name:
+            rgb = observation[sensor_name]
+            if not isinstance(rgb, np.ndarray):
+                rgb = rgb.cpu().numpy()
+
+            render_obs_images.append(rgb)
+        elif "depth" in sensor_name:
+            depth_map = observation[sensor_name].squeeze() * 255.0
+            if not isinstance(depth_map, np.ndarray):
+                depth_map = depth_map.cpu().numpy()
+
+            depth_map = depth_map.astype(np.uint8)
+            depth_map = np.stack([depth_map for _ in range(3)], axis=2)
+            render_obs_images.append(depth_map)
+
+    # add image goal if observation has image_goal info
+    if "imagegoal" in observation or "imagegoalrotation" in observation:
+        if "imagegoal" in observation:
+            rgb = observation["imagegoal"]
+        else:
+            rgb = observation["imagegoalrotation"]
+        if not isinstance(rgb, np.ndarray):
+            rgb = rgb.cpu().numpy()
+
+        render_obs_images.append(rgb)
+
+    assert (
+        len(render_obs_images) > 0
+    ), "Expected at least one visual sensor enabled."
+
+    shapes_are_equal = len(set(x.shape for x in render_obs_images)) == 1
+    if not shapes_are_equal:
+        render_frame = tile_images(render_obs_images)
+    else:
+        render_frame = np.concatenate(render_obs_images, axis=1)
+
+    # draw collision
+    if "collisions" in info and info["collisions"]["is_collision"]:
+        render_frame = draw_collision(render_frame)
+
+    if "top_down_map" in info:
+        top_down_map = maps.colorize_draw_agent_and_fit_to_height(
+            info["top_down_map"], render_frame.shape[0]
+        )
+        render_frame = np.concatenate((render_frame, top_down_map), axis=1)
+    return render_frame
+
+
 def generate_video(
     video_option: List[str],
     video_dir: Optional[str],
@@ -161,6 +229,17 @@ def generate_video(
         assert video_dir is not None
         images_to_video(images, video_dir, video_name, verbose=verbose)
     if "wandb" in video_option:
+        images = np.array(images)
         images = images.transpose(0, 3, 1, 2)
         wandb.log({f"episode{episode_id}_{checkpoint_idx}": wandb.Video(images, fps=fps)})
 
+def add_info_to_image(frame, info):
+    string = "d2g: {} | a2g: {} |\nsimple reward: {} |\nsuccess: {} | angle success: {}".format(
+        round(info["distance_to_goal"], 3),
+        round(info["angle_to_goal"], 3),
+        round(info["simple_reward"], 3),
+        round(info["success"], 3),
+        round(info["angle_success"], 3),
+    )
+    frame = append_text_to_image(frame, string)
+    return frame
