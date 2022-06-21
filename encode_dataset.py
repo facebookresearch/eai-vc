524fca0faa8eaf1fc1ea6cd4637969b71e0a0426
Nicklas Hansen <hello@nicklashansen.com>
2022-06-20 18:54:40 -0700

    support resnet18/50 dataset encodings

--- encode_dataset.py
+++ encode_dataset.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import warnings
 warnings.filterwarnings('ignore')
 import os
@@ -6,6 +7,7 @@ os.environ['MUJOCO_GL'] = 'egl'
 import torch
 import numpy as np
 from pathlib import Path
+from collections import defaultdict
 from PIL import Image
 from cfg_parse import parse_cfg
 from dataloader import DMControlDataset
@@ -24,14 +26,34 @@ __ENCODER__ = None
 __PREPROCESS__ = None
 
 
+def make_encoder(cfg):
+	"""Make an encoder."""
+	assert torch.cuda.is_available(), 'CUDA is not available'
+	assert cfg.get('features', None) is not None, 'Features must be specified'
+	if cfg.features == 'clip':
+		import clip
+		return clip.load('ViT-B/32', device='cuda') # 151M params
+	# resnet50: 24M params
+	# resnet18: 11M params
+	encoder = torchvision.models.__dict__['resnet' + str(18 if '18' in cfg.features else 50)](pretrained=False).cuda()
+	if cfg.features == 'moco':
+		state_dict = torch.load(os.path.join(cfg.encoder_dir, 'moco_v2_800ep_pretrain.pth.tar'))['state_dict']
+		state_dict = {k.replace('module.encoder_q.', ''): v for k,v in state_dict.items() if not k.startswith('fc.')}
+		encoder.load_state_dict(state_dict, strict=False)
+	encoder.fc = nn.Identity()
+	encoder.eval()
+	preprocess = nn.Sequential(
+		K.Resize((252, 252), resample=Resample.BILINEAR), K.RandomCrop((224, 224)),
+		K.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])).cuda()
+
+	return encoder, preprocess
+
+
 def encode_clip(obs, cfg):
 	"""Encode one or more observations using CLIP."""
 	global __ENCODER__, __PREPROCESS__
-	assert torch.cuda.is_available(), 'CUDA is not available'
 	if __ENCODER__ is None:
-		print('Loading CLIP model...')
-		import clip
-		__ENCODER__, __PREPROCESS__ = clip.load('ViT-B/32', device='cuda') # 151M params
+		__ENCODER__, __PREPROCESS__ = make_encoder(cfg)
 	assert isinstance(obs, (torch.Tensor, np.ndarray)), 'Observation must be a tensor or numpy array'
 	if isinstance(obs, torch.Tensor):
 		obs = obs.cpu().numpy()
@@ -53,22 +75,11 @@ def encode_clip(obs, cfg):
 	return features
 
 
-def encode_moco(obs, cfg):
-	"""Encode one or more observations using MoCo-v2."""
+def encode_resnet(obs, cfg):
+	"""Encode one or more observations using a ResNet model."""
 	global __ENCODER__, __PREPROCESS__
-	assert torch.cuda.is_available(), 'CUDA is not available'
 	if __ENCODER__ is None:
-		__ENCODER__ = torchvision.models.__dict__['resnet50']().cuda() # 24M params
-		state_dict = torch.load(os.path.join(cfg.encoder_dir, 'moco_v2_800ep_pretrain.pth.tar'))['state_dict']
-		state_dict = {k.replace('module.encoder_q.', ''): v for k,v in state_dict.items() if not k.startswith('fc.')}
-		__ENCODER__.load_state_dict(state_dict, strict=False)
-		__ENCODER__.fc = nn.Identity()
-		__ENCODER__.eval()
-		__PREPROCESS__ = nn.Sequential(
-			K.Resize((252, 252), resample=Resample.BILINEAR),
-			K.RandomCrop((224, 224)),
-			K.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-		).cuda()
+		__ENCODER__, __PREPROCESS__ = make_encoder(cfg)
 	assert isinstance(obs, (torch.Tensor, np.ndarray)), 'Observation must be a tensor or numpy array'
 	if isinstance(obs, np.ndarray):
 		obs = torch.from_numpy(obs)
@@ -88,8 +99,7 @@ def encode_moco(obs, cfg):
 @hydra.main(config_name='default', config_path='config')
 def encode(cfg: dict):
 	"""Encoding an image dataset using a pretrained model."""
-	assert torch.cuda.is_available()
-	assert 'features' in cfg, 'Features must be specified'
+	assert cfg.get('features', None), 'Features must be specified'
 	cfg.modality = 'pixels'
 	cfg = parse_cfg(cfg)
 	from env import make_env
@@ -99,12 +109,10 @@ def encode(cfg: dict):
 	# Load dataset
 	tasks = _env.unwrapped.tasks if cfg.get('multitask', False) else [cfg.task]
 	dataset = DMControlDataset(cfg, Path(cfg.data_dir) / 'dmcontrol', tasks=tasks, fraction=cfg.fraction)
-	features_to_fn = {
-		'clip': encode_clip,
-		'moco': encode_moco
-	}
+	features_to_fn = defaultdict(lambda: encode_resnet)
+	features_to_fn.update({'clip': encode_clip})
 	fn = features_to_fn[cfg.features]
-	
+
 	# Encode dataset
 	for episode in tqdm(dataset.episodes):
 		
