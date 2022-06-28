d86b51141c1bb8549f82fa99a59529cdef9643ca
Claire Chen <clairelchen@fb.com>
2022-06-27 17:46:26 -0700

    Save actions to observations

--- sim_move_cube.py
+++ sim_move_cube.py
@@ -17,6 +17,7 @@ sys.path.insert(0, os.path.join(base_path, '..'))
 from envs.cube_env import SimCubeEnv, ActionType
 from policies.move_cube_policy import MoveCubePolicy
 
+SIM_TIME_STEP = 0.004
 
 def main(args):
     env = SimCubeEnv(
@@ -27,30 +28,28 @@ def main(args):
         difficulty=args.difficulty,
         enable_cameras=True,
         finger_type="trifingerpro",
+        time_step=SIM_TIME_STEP,
     )
-
-    policy = MoveCubePolicy(env.action_space, env.platform)
       
     if args.log_paths:
         num_episodes = len(args.log_paths)
     else:
         num_episodes = 6
 
-    t = 0
     for i in range(num_episodes):
         print(f"Running episode {i}")
 
         is_done = False
-        env.reset()
+
+        observation = env.reset()
+        policy = MoveCubePolicy(env.action_space, env.platform, time_step=SIM_TIME_STEP)
         policy.reset()
 
         observation_list = []
-        observation = env._create_observation(t, env._initial_action)
 
         while not is_done:
             action = policy.predict(observation)
             observation, reward, episode_done, info = env.step(action)
-            t = info["time_index"]
 
             policy_observation = policy.get_observation()
 
@@ -61,9 +60,28 @@ def main(args):
             if args.log_paths is not None: observation_list.append(full_observation)
 
         if args.log_paths is not None:
+            # Compute actions (ftpos and joint state deltas) across trajectory
+            add_actions_to_obs(observation_list) 
             log_path = args.log_paths[i]
             np.savez_compressed(log_path, data=observation_list)
             print(f"Saved episode {i} to {log_path}")
+
+def add_actions_to_obs(observation_list):
+
+    for t in range(len(observation_list) - 1):
+        ftpos_cur  = observation_list[t]["policy"]["controller"]["ft_pos_cur"]
+        ftpos_next = observation_list[t+1]["policy"]["controller"]["ft_pos_cur"]
+        delta_ftpos = ftpos_next - ftpos_cur
+
+        q_cur  = observation_list[t]["robot_observation"]["position"]
+        q_next = observation_list[t+1]["robot_observation"]["position"]
+        delta_q = q_next - q_cur
+
+        action_dict = {"delta_ftpos": delta_ftpos, "delta_q": delta_q}
+        observation_list[t]["action"] = action_dict
+        
+    action_dict = {"delta_ftpos": np.zeros(delta_ftpos.shape), "delta_q": np.zeros(delta_q.shape)}
+    observation_list[-1]["action"] = action_dict
 
 def parse_args():
     parser = argparse.ArgumentParser()
