50d379cc4fadf391c79f7fd146cb2158707c92dd
Claire Chen <clairelchen@fb.com>
2022-07-06 11:08:19 -0700

    Change save dir to viz_sim, fix saving bug

--- scripts/viz_sim_log.py
+++ scripts/viz_sim_log.py
@@ -19,10 +19,13 @@ def main(args):
     traj_dict = d_utils.get_traj_dict_from_obs_list(data)
 
     #position_error = np.array([data[i]["achieved_goal"]["position_error"] for i in range(len(data))])
-    o_cur = traj_dict["o_cur"] # object position, observed
-    o_des = traj_dict["o_des"] # object position, desired
+    o_cur = traj_dict["o_cur_pos"] # object position, observed
+    o_des = traj_dict["o_des_pos"] # object position, desired
     ft_pos_cur = traj_dict["ft_pos_cur"] # ft position, actual
     ft_pos_des = traj_dict["ft_pos_des"] # ft position, desired
+
+    o_cur_ori = traj_dict["o_cur_ori"] # object ori observed
+    o_des_ori = traj_dict["o_des_ori"] # object ori desired
 
     downsampled_traj_dict = d_utils.downsample_traj_dict(traj_dict)
     
@@ -30,30 +33,41 @@ def main(args):
     if args.save:
         demo_name = os.path.splitext(os.path.split(file_path)[1])[0]
         demo_dir = os.path.split(file_path)[0]
-        out_dir = os.path.join(demo_dir, "viz", demo_name)
+        out_dir = os.path.join(demo_dir, "sim_viz", demo_name)
         if not os.path.exists(out_dir): os.makedirs(out_dir, exist_ok=False)
     else:
-        out_path = None
+        out_dir = None
 
     ## Plot ft positions
     d_utils.plot_traj(
             "ft position", 
-            os.path.join(out_dir, "ft_pos.png"),
+            (os.path.join(out_dir, "ft_pos.png") if out_dir else None),
             ["x1", "y1", "z1", "x2", "y2", "z2", "x3", "y3", "z3",],
             {
-            "all":  {"y": traj_dict["ft_pos_des"], "x": traj_dict["t"]},
-            "down": {"y": downsampled_traj_dict["ft_pos_cur"], "x": downsampled_traj_dict["t"], "marker": "x"},
+            "desired":  {"y": traj_dict["ft_pos_des"], "x": traj_dict["t"]},
+            "actual":  {"y": traj_dict["ft_pos_cur"], "x": traj_dict["t"]},
+            #"down": {"y": downsampled_traj_dict["ft_pos_cur"], "x": downsampled_traj_dict["t"], "marker": "x"},
             }
             )
 
     ## Plot object positions
     d_utils.plot_traj(
             "object position", 
-            os.path.join(out_dir, "obj_pos.png"),
+            (os.path.join(out_dir, "obj_pos.png") if out_dir else None),
             ["x", "y", "z"],
             {
             "actual":  {"y": o_cur, "x": traj_dict["t"]},
             "desired": {"y": o_des, "x": traj_dict["t"]},
+            }
+            )
+
+    ## Plot object positions
+    d_utils.plot_traj(
+            "object ori", 
+            (os.path.join(out_dir, "obj_ori.png") if out_dir else None),
+            ["x", "y", "z", "w"],
+            {
+            "actual":  {"y": o_cur_ori, "x": traj_dict["t"]},
             }
             )
 
