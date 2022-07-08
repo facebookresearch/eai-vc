4791fb2179c94ba92097824ff73ca6a4caae9fa7
Nicklas Hansen <hello@nicklashansen.com>
2022-07-07 23:13:56 -0700

    add stylized barplot

--- plot_singletask.py
+++ plot_singletask.py
@@ -6,26 +6,23 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
-# matplotlib.rcParams['pdf.fonttype'] = 42
-# matplotlib.rcParams['ps.fonttype'] = 42
-# DEFAULT_PLOT_RC = {
-# 	'grid.linestyle': ':',
-# 	'grid.linewidth': 2.5,
-# 	'axes.labelsize': 14,
-# 	'axes.titlesize': 16,
-# 	'axes.facecolor': '#F6F6F6',
-# 	'axes.edgecolor': '#333',
-# 	'legend.fontsize': 14,
-# 	'xtick.labelsize': 14,
-# 	'xtick.bottom': True,
-# 	'xtick.color': '#333',
-# 	'ytick.labelsize': 24,
-# 	'ytick.left': True,
-# 	'ytick.color': '#333',
-# }
-# sns.set(style='whitegrid', rc=DEFAULT_PLOT_RC)
-sns.set(style='white', rc={'figure.figsize':(18,10)})
-sns.set_context('paper')
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+DEFAULT_PLOT_RC = {
+	'axes.labelsize': 14,
+	'axes.titlesize': 20,
+	'axes.facecolor': '#F6F6F6',
+	'axes.edgecolor': '#333',
+	'legend.fontsize': 14,
+	'xtick.labelsize': 14,
+	'xtick.bottom': True,
+	'xtick.color': '#333',
+	'ytick.labelsize': 16,
+	'ytick.left': True,
+	'ytick.color': '#333',
+}
+sns.set(style='whitegrid', rc=DEFAULT_PLOT_RC)
+# sns.set_context('paper')
 ENTITY = 'nihansen'
 PROJECT = 'tdmpc2'
 
@@ -36,10 +33,20 @@ def main():
 		'metaworld': ['mw-drawer-close', 'mw-hammer', 'mw-box-close', 'mw-pick-place', 'mw-shelf-place']
 	}
 	exp_names = ['v1', 'offline-v1', 'mocoego-v1', 'mocoego-offline-v1', 'random-v1', 'random-offline-v1']
+	experiment2label = {
+		'state-v1': 'State',
+		'state-offline-v1': '✻ State',
+		'pixels-v1': 'Pixels',
+		'pixels-offline-v1': '✻ Pixels',
+		'features-mocoego-v1': 'Ego4D',
+		'features-mocoego-offline-v1': '✻ Ego4D',
+		'features-random-v1': 'Random',
+		'features-random-offline-v1': '✻ Random',
+	}
 	num_seeds = 3
 	seeds = set(range(1, num_seeds+1))
 
-	api = wandb.Api(timeout=60)
+	api = wandb.Api(timeout=100)
 	runs = api.runs(
 		os.path.join(ENTITY, PROJECT),
 		filters = {
@@ -64,28 +71,70 @@ def main():
 			continue
 		key = 'offline/reward' if 'offline' in exp_name else 'eval/episode_reward'
 		hist = run.history(keys=[key], x_axis='_step')
-		if len(hist) < 3:
+		if len(hist) < 1:
 			continue
 		experiment = cfg['modality'] + '-' + exp_name
+		label = experiment2label[experiment]
+		idx = list(experiment2label.values()).index(label) 
 		reward = np.array(hist[key])[-1]
-		print(f'Appending experiment {experiment} with reward {reward}')
+		print(f'Appending experiment {label} with reward {reward}')
 		entries.append(
-			(cfg['task'], experiment, seed, reward)
+			(idx, cfg['task'], label, seed, reward)
 		)
 	
-	df = pd.DataFrame(entries, columns=['task', 'experiment', 'seed', 'reward'])
+	df = pd.DataFrame(entries, columns=['idx', 'task', 'experiment', 'seed', 'reward'])
 	df_dmcontrol = df[df['task'].isin(tasks['dmcontrol'])]
 	df_metaworld = df[df['task'].isin(tasks['metaworld'])]
 
-	g = sns.catplot(data=df_dmcontrol, kind='bar', x='task', y='reward', hue='experiment', ci=95)
-	g.set_axis_labels('', 'Episode return')
-	g.set(ylim=(0, 1000))
-	g.savefig(f'singletask-dmcontrol.png')
+	# average across tasks
+	df_dmcontrol = df_dmcontrol.groupby(['idx', 'experiment', 'seed']).mean().reset_index()
+	df_metaworld = df_metaworld.groupby(['idx', 'experiment', 'seed']).mean().reset_index()
 
-	g = sns.catplot(data=df_metaworld, kind='bar', x='task', y='reward', hue='experiment', ci=95)
-	g.set_axis_labels('', 'Episode return')
-	g.set(ylim=(0, 5000))
-	g.savefig(f'singletask-metaworld.png')
+	# average across seeds
+	df_dmcontrol = df_dmcontrol.groupby(['idx', 'experiment']).mean().reset_index()
+	df_metaworld = df_metaworld.groupby(['idx', 'experiment']).mean().reset_index()
+
+	# rescale reward
+	df_dmcontrol['reward'] = (df_dmcontrol['reward'] / 10).round()
+	df_metaworld['reward'] = (df_metaworld['reward'] / 45).round()
+
+	f, axs = plt.subplots(1, 2, figsize=(16,6))
+
+	# color palette
+	colors = sns.color_palette('colorblind', 4)
+
+	# dmcontrol
+	ax = axs[0]
+	sns.barplot(data=df_dmcontrol, x='experiment', y='reward', ax=ax, ci=None)
+	ax.set_title('DMControl', fontweight='bold')
+	ax.set_ylim(0, 100)
+	ax.set_xlabel('')
+	ax.set_ylabel('Normalized return')
+	ax.bar_label(ax.containers[0], fontsize=18)
+	ax.tick_params(labelrotation=35)
+	for i in range(1, len(df_dmcontrol), 2):
+		ax.containers[0].patches[i]._hatch = '//'
+		ax.containers[0].patches[i].set_facecolor(ax.containers[0].patches[i-1]._facecolor)
+	
+	# metaworld
+	ax = axs[1]
+	sns.barplot(data=df_metaworld, x='experiment', y='reward', ax=ax, ci=None)
+	ax.set_title('Meta-World', fontweight='bold')
+	ax.set_ylim(0, 100)
+	ax.set_xlabel('')
+	ax.set_ylabel('')
+	ax.bar_label(ax.containers[0], fontsize=18)
+	ax.tick_params(labelrotation=35)
+	for i in range(1, len(df_metaworld), 2):
+		ax.containers[0].patches[i]._hatch = '//'
+		ax.containers[0].patches[i].set_facecolor(ax.containers[0].patches[i-1]._facecolor)
+
+	h, l = axs[0].get_legend_handles_labels()
+	f.legend(h, l, loc='lower center', ncol=4, frameon=False)
+	plt.tight_layout()
+	f.subplots_adjust(bottom=0.16, wspace=0.15)
+	plt.savefig('plot.png', bbox_inches='tight')
+
 
 
 
--- train_offline_metaworld_features.sh
+++ train_offline_metaworld_features.sh
@@ -1,10 +1,10 @@
 python train_offline.py \
-    task=mw-drawer-close \
+    -m task=mw-drawer-close,mw-drawer-open,mw-hammer,mw-box-close,mw-pick-place,mw-shelf-place \
     modality=features \
-    features=mocoego \
+    features=mocoego,random \
     frame_stack=1 \
     action_repeat=2 \
     episode_length=250 \
-    exp_name=test \
-    seed=1 \
+    exp_name=offline-v1 \
+    seed=1,2,3 \
     hydra/launcher=slurm
