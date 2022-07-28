59aa1d023ceda1bfa953bf917e6e9026f904fc09
Claire Chen <clairelchen@fb.com>
2022-07-27 17:28:01 -0700

    Use r3m forward model in mbirl

--- trifinger_mbirl/mbirl.py
+++ trifinger_mbirl/mbirl.py
@@ -10,8 +10,6 @@ import argparse
 import collections
 import wandb
 
-#import trifinger_simulation.finger_types_data
-
 base_path = os.path.dirname(__file__)
 sys.path.insert(0, base_path)
 sys.path.insert(0, os.path.join(base_path, '..'))
@@ -25,13 +23,16 @@ from trifinger_mbirl.two_phase_mpc import TwoPhaseMPC
 # The IRL Loss, the learning objective for the learnable cost functions.
 # Measures the distance between the demonstrated fingertip position trajectory and predicted trajectory
 class IRLLoss(object):
-    def __init__(self, irl_loss_state):
+    def __init__(self, irl_loss_state, obj_state_type):
         self.irl_loss_state = irl_loss_state
+        self.obj_state_type = obj_state_type
 
     def __call__(self, full_pred_traj, expert_demo_dict, dist_scale=1):
         pred_traj = d_utils.parse_pred_traj(full_pred_traj, self.irl_loss_state)
-        target_traj = get_expert_demo(expert_demo_dict, self.irl_loss_state)
-        loss = ((pred_traj * dist_scale - target_traj * dist_scale) ** 2).sum(dim=0)
+        target_traj = get_expert_demo(expert_demo_dict, self.irl_loss_state, self.obj_state_type)
+        loss = ((pred_traj * dist_scale - target_traj * dist_scale) ** 2).sum(dim=1)
+        #print(loss.shape)
+        #quit()
         return loss.mean()
 
 def plot_loss(loss_dict, outer_i):
@@ -40,6 +41,7 @@ def plot_loss(loss_dict, outer_i):
     log_dict = {f'{k}': v for k, v in loss_dict.items()}
     log_dict['outer_i'] = outer_i
     wandb.log(log_dict)
+
 
 def evaluate_action_optimization(conf, learned_cost, irl_loss_fn, mpc, trajs, plots_dir=None, outer_i=None):
     """ Test current learned cost by running inner loop action optimization on test demonstrations """
@@ -51,7 +53,7 @@ def evaluate_action_optimization(conf, learned_cost, irl_loss_fn, mpc, trajs, pl
 
     for t_i, expert_demo_dict in enumerate(trajs):
  
-        obs_dict_init = d_utils.get_obs_dict_from_traj(expert_demo_dict, 0, "pos") # Initial state
+        obs_dict_init = d_utils.get_obs_dict_from_traj(expert_demo_dict, 0, conf.obj_state_type) # Initial state
 
         # Reset mpc for action optimization
         mpc.reset_actions()
@@ -64,7 +66,7 @@ def evaluate_action_optimization(conf, learned_cost, irl_loss_fn, mpc, trajs, pl
             pred_traj = mpc.roll_out(obs_dict_init.copy())
 
             # use the learned loss to update the action sequence
-            target = get_target_for_cost_type(expert_demo_dict, conf.cost_type, conf.cost_state)
+            target = get_target_for_cost_type(expert_demo_dict, conf.cost_type, conf.cost_state, conf.obj_state_type)
             pred_traj_for_cost = d_utils.parse_pred_traj(pred_traj, conf.cost_state)
             learned_cost_val = learned_cost(pred_traj_for_cost, target)
             learned_cost_val.backward(retain_graph=True)
@@ -78,7 +80,7 @@ def evaluate_action_optimization(conf, learned_cost, irl_loss_fn, mpc, trajs, pl
 
         # Plot predicted trajectories
         if plots_dir is not None:
-            plot_traj(plots_dir, outer_i, t_i, pred_state_traj_new, expert_demo_dict, conf.mpc_type)
+            plot_traj(plots_dir, outer_i, t_i, pred_state_traj_new, expert_demo_dict, conf.mpc_type, conf.obj_state_type)
 
     return torch.stack(eval_costs).detach(), test_pred_trajs
 
@@ -113,7 +115,7 @@ def train(conf, learnable_cost, irl_loss_fn, mpc, train_trajs, test_trajs,
             learnable_cost_opt.zero_grad()
             expert_demo_dict = train_trajs[demo_i]
 
-            obs_dict_init = d_utils.get_obs_dict_from_traj(expert_demo_dict, 0, "pos") # Initial state
+            obs_dict_init = d_utils.get_obs_dict_from_traj(expert_demo_dict, 0, conf.obj_state_type) # Initial state
 
             # Reset mpc for action optimization
             mpc.reset_actions()
@@ -124,7 +126,7 @@ def train(conf, learnable_cost, irl_loss_fn, mpc, train_trajs, test_trajs,
                 for i in range(conf.n_inner_iter):
                     pred_traj = fpolicy.roll_out(obs_dict_init.copy())
                     # use the learned loss to update the action sequence
-                    target = get_target_for_cost_type(expert_demo_dict, conf.cost_type, conf.cost_state)
+                    target = get_target_for_cost_type(expert_demo_dict, conf.cost_type, conf.cost_state, conf.obj_state_type)
                     pred_traj_for_cost = d_utils.parse_pred_traj(pred_traj, conf.cost_state)
                     learned_cost_val = learnable_cost(pred_traj_for_cost, target)
                     diffopt.step(learned_cost_val)
@@ -146,7 +148,7 @@ def train(conf, learnable_cost, irl_loss_fn, mpc, train_trajs, test_trajs,
 
             # Plot
             if (outer_i+1) % conf.n_epoch_every_log == 0:
-                plot_traj(plots_dir, outer_i, demo_i, pred_traj, expert_demo_dict, conf.mpc_type)
+                plot_traj(plots_dir, outer_i, demo_i, pred_traj, expert_demo_dict, conf.mpc_type, conf.obj_state_type)
                 pred_actions = fpolicy.action_seq.data.detach().numpy()
                 plot_actions(plots_dir, outer_i, demo_i, pred_actions, expert_demo_dict)
 
@@ -236,10 +238,10 @@ def train(conf, learnable_cost, irl_loss_fn, mpc, train_trajs, test_trajs,
             }, f=f'{ckpts_dir}/epoch_{outer_i+1}_ckpt.pth')
 
 
-def get_target_for_cost_type(demo_dict, cost_type, cost_state):
+def get_target_for_cost_type(demo_dict, cost_type, cost_state, obj_state_type):
     """ Get target from traj_dict for learnable cost function given cost_type  and cost_state """
 
-    expert_demo = get_expert_demo(demo_dict, cost_state)
+    expert_demo = get_expert_demo(demo_dict, cost_state, obj_state_type)
     ft_pos_targets_per_mode = torch.Tensor(demo_dict["ft_pos_targets_per_mode"])
 
     if cost_type in ['Weighted', 'TimeDep', 'RBF']:
@@ -252,7 +254,7 @@ def get_target_for_cost_type(demo_dict, cost_type, cost_state):
         elif cost_state in ["obj", "ftpos_obj"]:
             # Get init and goal object pose 
             # TODO for now, just get first and last state from demo traj; eventually use goal?? How to do when not using obj pos?
-            init = expert_demo[0, :]
+            init = expert_demo[7, :] # Last ind in phase 1 TODO hack
             goal = expert_demo[-1, :]
             target = torch.stack([init, goal])
         else:
@@ -262,16 +264,29 @@ def get_target_for_cost_type(demo_dict, cost_type, cost_state):
 
     return target
 
-def get_expert_demo(traj_dict, expert_demo_state):
+def get_expert_demo(traj_dict, expert_demo_state, obj_state_type):
     """ Get expert demo from traj_dict, given expert_demo_state """
 
     if expert_demo_state == "ftpos":
         expert_demo = torch.Tensor(traj_dict["ft_pos_cur"])
     elif expert_demo_state == "ftpos_obj":
+        if obj_state_type == "pos":
+            o_expert_demo = torch.Tensor(traj_dict["o_pos_cur"])
+        elif obj_state_type == "img_r3m":
+            o_expert_demo = torch.Tensor(traj_dict["image_60_r3m"])
+        else:
+            raise ValueError
+
         expert_demo = torch.cat([torch.Tensor(traj_dict["ft_pos_cur"]),\
-                                 torch.Tensor(traj_dict["o_pos_cur"])], dim=1)
+                                 torch.Tensor(o_expert_demo)], dim=1)
+
     elif expert_demo_state == "obj":
-        expert_demo = torch.Tensor(traj_dict["o_pos_cur"])
+        if obj_state_type == "pos":
+            expert_demo = torch.Tensor(traj_dict["o_pos_cur"])
+        elif obj_state_type == "img_r3m":
+            expert_demo = torch.Tensor(traj_dict["image_60_r3m"])
+        else:
+            raise ValueError
     else:
         raise ValueError(f"{expert_demo_state} is invalid expert_demo_state")
     return expert_demo
@@ -288,7 +303,9 @@ def get_mpc(mpc_type, time_horizon):
         #phase2_model_path = "/Users/clairelchen/projects/trifinger_claire/trifinger_mbirl/forward_models/runs/phase2_model_nt-100_ost-pos_train-m2/epoch_1500_ckpt.pth"
 
         ## Phase 2 model trained with full demos
-        phase2_model_path = "trifinger_mbirl/forward_models/runs/phase2_model_nt-100_ost-pos_train-all/epoch_3000_ckpt.pth"
+        #phase2_model_path = "trifinger_mbirl/forward_models/runs/phase2_model_nt-100_ost-pos_train-all/epoch_3000_ckpt.pth"
+
+        phase2_model_path = "trifinger_mbirl/forward_models/runs/phase2_model_nt-100_ost-img_r3m/epoch_5000_ckpt.pth"
 
         if "learned_only" in mpc_type: learned_only = True
         else: learned_only = False
@@ -304,9 +321,20 @@ def get_learnable_cost(conf, time_horizon):
     rbf_width    = conf.rbf_width
     cost_type    = conf.cost_type
 
-    # Set dimension for cost function, based on cost_state
     ftpos_dim = 9
-    o_state_dim = 3 # TODO hardcoded
+    
+       
+    # Set object state dim
+    if conf.obj_state_type == "pos":
+        o_state_dim = 3 # TODO hardcoded
+    elif conf.obj_state_type == "vertices":
+        o_state_dim = 8*3 # TODO hardcoded
+    elif conf.obj_state_type == "img_r3m":
+        o_state_dim = 2048 # TODO hardcoded
+    else:
+        raise ValueError("Invalid obj_state_type") 
+
+    # Set dimension for cost function, based on cost_state
     if conf.cost_state == "ftpos":
         n_keypt_dim = ftpos_dim
     elif conf.cost_state == "obj":
@@ -334,7 +362,7 @@ def get_learnable_cost(conf, time_horizon):
     return learnable_cost
 
 
-def plot_traj(plots_dir, outer_i, demo_i, pred_traj, expert_demo_dict, mpc_type):
+def plot_traj(plots_dir, outer_i, demo_i, pred_traj, expert_demo_dict, mpc_type, obj_state_type):
     """ Plot predicted and expert trajectories, based on mpc_type """
 
     traj_dir = os.path.join(plots_dir, f"traj_{demo_i}")
@@ -344,12 +372,12 @@ def plot_traj(plots_dir, outer_i, demo_i, pred_traj, expert_demo_dict, mpc_type)
     save_name = f"epoch_{outer_i+1}.png"
     save_path = os.path.join(traj_dir, save_name)
 
-    if "ftpos_obj" in mpc_type:
+    if "ftpos_obj" in mpc_type and obj_state_type == "pos": # TODO hardcoded
         d_list = ["x1", "y1", "z1", "x2", "y2", "z2", "x3", "y3", "z3", "ox", "oy", "oz"]
-        plot_expert_demo = get_expert_demo(expert_demo_dict, "ftpos_obj")
+        plot_expert_demo = get_expert_demo(expert_demo_dict, "ftpos_obj", obj_state_type)
     else:
         d_list = ["x1", "y1", "z1", "x2", "y2", "z2", "x3", "y3", "z3"]
-        plot_expert_demo = get_expert_demo(expert_demo_dict, "ftpos")
+        plot_expert_demo = get_expert_demo(expert_demo_dict, "ftpos", obj_state_type)
 
     d_utils.plot_traj(
             title, 
