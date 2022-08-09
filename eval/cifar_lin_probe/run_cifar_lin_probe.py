2701abca4c648f41c4348f7c4b280b5cdb50c1d3
1heart <yixin1996@gmail.com>
2022-08-09 13:53:34 -0400

    Remove breakpoint

--- eval/cifar_lin_probe/run_cifar_lin_probe.py
+++ eval/cifar_lin_probe/run_cifar_lin_probe.py
@@ -78,8 +78,6 @@ def probe_model_eval(config):
     # Get base model, transform, and probing classifier
     model, embedding_dim, transform = hydra.utils.call(config["model"])
 
-    breakpoint()
-
     probe = torch.nn.Sequential(torch.nn.BatchNorm1d(embedding_dim),
                                 torch.nn.Linear(embedding_dim, 10))
 
