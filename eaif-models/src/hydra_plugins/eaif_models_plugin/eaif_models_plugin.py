0389e67df5e6241f8c78f68a49cbb662a02471c3
1heart <yixin1996@gmail.com>
2022-08-09 13:32:38 -0400

    Minor

--- eaif-models/src/hydra_plugins/eaif_models_plugin/eaif_models_plugin.py
+++ eaif-models/src/hydra_plugins/eaif_models_plugin/eaif_models_plugin.py
@@ -2,7 +2,7 @@ from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
 
 
-class PolymetisControllerServerPlugin(SearchPathPlugin):
+class EaifModelsPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         search_path.append(
             provider="eaif-models-plugin",
