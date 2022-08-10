f2ee7e8e092ba382809610a4d7c5513883dac6e2
1heart <yixin1996@gmail.com>
2022-08-10 10:54:25 -0400

    Debug tests

--- eaif-models/src/eaif_models/utils/wandb.py
+++ eaif-models/src/eaif_models/utils/wandb.py
@@ -5,16 +5,15 @@ log = logging.getLogger(__name__)
 
 def setup_wandb(project_name="eaif", entity="eai-foundations", resume=True):
     try:
+        log.info(f"wand initializing...")
         import wandb
-    except ImportError as e:
-        log.warning(f"Cannot import wandb: {e}")
+
+        wandb.require("service")
+        wandb.init(project=project_name, entity=entity, resume=resume)
+
+        log.info(f"wandb initialized")
+
+        return wandb
+    except Exception as e:
+        log.warning(f"Cannot initialize wandb: {e}")
         return
-
-    log.info(f"wand initializing...")
-
-    wandb.require("service")
-    wandb.init(project=project_name, entity=entity, resume=resume)
-
-    log.info(f"wandb initialized")
-
-    return wandb
