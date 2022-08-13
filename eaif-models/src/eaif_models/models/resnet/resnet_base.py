ab7a10b02fbab1214f29331deadbe9bc95139e19
1heart <yixin1996@gmail.com>
2022-08-13 10:58:22 -0400

    Fix load_resnet --> load_model

--- eaif-models/src/eaif_models/models/resnet/resnet_base.py
+++ eaif-models/src/eaif_models/models/resnet/resnet_base.py
@@ -1,4 +1,4 @@
-import torch, torch.nn as nn
+import torch
 import torchvision.models as models, torchvision.transforms as T
 
 _resnet_transforms = T.Compose([
@@ -8,8 +8,7 @@ _resnet_transforms = T.Compose([
                         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                     ])
 
-def load_resnet(version='resnet50', imagenet_pretrained=True, metadata=None,
-                *args, **kwargs):
+def load_model(version='resnet50', imagenet_pretrained=True, metadata=None):
     """
     Load a resnet model with random weights, imagenet pretrained weights, or from a checkpoint.
     """
@@ -22,4 +21,4 @@ def load_resnet(version='resnet50', imagenet_pretrained=True, metadata=None,
         model = models.resnet18(pretrained=imagenet_pretrained, progress=False)
     model.fc = torch.nn.modules.linear.Identity()
     embedding_dim, transforms = 2048, _resnet_transforms
-    return model, embedding_dim, _resnet_transforms, metadata
+    return model, embedding_dim, transforms, metadata
