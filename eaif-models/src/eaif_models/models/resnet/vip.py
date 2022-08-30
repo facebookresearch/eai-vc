d08ae0aacdb1890d2e2ee266940390d71af7865a
Yixin Lin <1heart@users.noreply.github.com>
2022-08-30 13:56:36 -0400

    Fix code style issues with Black (#26)
    Co-authored-by: bot <bot@fb.com>

--- eaif-models/src/eaif_models/models/resnet/vip.py
+++ eaif-models/src/eaif_models/models/resnet/vip.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import omegaconf
 import hydra
 import copy
@@ -13,13 +13,28 @@ import torchvision.transforms as T
 from torchvision.transforms import InterpolationMode
 
 
-VALID_ARGS = ["_target_", "device", "lr", "hidden_dim", "size", "l2weight", "l1weight", "langweight", "tcnweight", "gofarweight", "gamma", "l2dist", "bs"]
+VALID_ARGS = [
+    "_target_",
+    "device",
+    "lr",
+    "hidden_dim",
+    "size",
+    "l2weight",
+    "l1weight",
+    "langweight",
+    "tcnweight",
+    "gofarweight",
+    "gamma",
+    "l2dist",
+    "bs",
+]
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
 
 vip_def_path = "eaif_models.models.resnet.vip.VIP"
+
 
 def cleanup_config(cfg):
     config = copy.deepcopy(cfg)
@@ -29,11 +44,12 @@ def cleanup_config(cfg):
             del config.agent[key]
     config.agent["_target_"] = vip_def_path
     config["device"] = device
-    
+
     ## Hardcodes to remove the language head
     ## Assumes downstream use is as visual representation
     config.agent["langweight"] = 0
     return config.agent
+
 
 def remove_language_head(state_dict):
     keys = state_dict.keys()
@@ -44,44 +60,66 @@ def remove_language_head(state_dict):
             del state_dict[key]
     return state_dict
 
+
 def load_vip(modelid, home):
     if modelid == "resnet50":
         foldername = "resnet50"
     else:
-        raise NameError('Invalid Model ID')
+        raise NameError("Invalid Model ID")
 
     modelpath = os.path.join(home, foldername, "model.pt")
     configpath = os.path.join(home, foldername, "config.yaml")
-    assert os.path.exists(os.path.join(home, foldername)), f"Must download vip models into {home}/{foldername}"
+    assert os.path.exists(
+        os.path.join(home, foldername)
+    ), f"Must download vip models into {home}/{foldername}"
 
     modelcfg = omegaconf.OmegaConf.load(configpath)
     cleancfg = cleanup_config(modelcfg)
     rep = hydra.utils.instantiate(cleancfg)
     rep = torch.nn.DataParallel(rep)
-    vip_state_dict = remove_language_head(torch.load(modelpath, map_location=torch.device(device))['r3m'])
+    vip_state_dict = remove_language_head(
+        torch.load(modelpath, map_location=torch.device(device))["r3m"]
+    )
     rep.load_state_dict(vip_state_dict)
     return rep
 
 
 epsilon = 1e-8
-def do_nothing(x): return x
+
+
+def do_nothing(x):
+    return x
+
 
 class VIP(nn.Module):
-    def __init__(self, device="cuda", lr=1e-4, hidden_dim=1024, size=50, l2weight=1.0, l1weight=1.0, 
-                 langweight=0.0, tcnweight=0.0, gofarweight=1.0, gamma=0.98, l2dist=True, bs=16):
+    def __init__(
+        self,
+        device="cuda",
+        lr=1e-4,
+        hidden_dim=1024,
+        size=50,
+        l2weight=1.0,
+        l1weight=1.0,
+        langweight=0.0,
+        tcnweight=0.0,
+        gofarweight=1.0,
+        gamma=0.98,
+        l2dist=True,
+        bs=16,
+    ):
         super().__init__()
 
         self.device = device
         self.use_tb = False
         self.l2weight = l2weight
         self.l1weight = l1weight
-        self.tcnweight = tcnweight ## Weight on TCN loss (states closer in same clip closer in embedding)
-        self.l2dist = l2dist ## Use -l2 or cosine sim
-        self.langweight = langweight ## Weight on language reward
+        self.tcnweight = tcnweight  ## Weight on TCN loss (states closer in same clip closer in embedding)
+        self.l2dist = l2dist  ## Use -l2 or cosine sim
+        self.langweight = langweight  ## Weight on language reward
         self.gofarweight = gofarweight
         self.hidden_dim = hidden_dim
         self.gamma = gamma
-        self.size = size ## Size ResNet or ViT
+        self.size = size  ## Size ResNet or ViT
         self.num_negatives = 3
 
         ## Distances and Metrics
@@ -105,51 +143,56 @@ class VIP(nn.Module):
         if self.size == 0:
             self.normlayer = T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
         else:
-            self.normlayer = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        if hidden_dim  > 0:
+            self.normlayer = T.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            )
+        if hidden_dim > 0:
             self.convnet.fc = nn.Linear(self.outdim, hidden_dim)
         else:
             self.convnet.fc = Identity()
         self.convnet.train()
         params += list(self.convnet.parameters())
         ## Optimizer
-        self.encoder_opt = torch.optim.Adam(params, lr = lr)
-
+        self.encoder_opt = torch.optim.Adam(params, lr=lr)
 
     ## Forward Call (im --> representation)
-    def forward(self, obs, obs_shape = [3, 224, 224]):
+    def forward(self, obs, obs_shape=[3, 224, 224]):
         if obs_shape != [3, 224, 224]:
             preprocess = nn.Sequential(
-                        T.Resize(256),
-                        T.CenterCrop(224),
-                        self.normlayer,
-                )
+                T.Resize(256),
+                T.CenterCrop(224),
+                self.normlayer,
+            )
         else:
             preprocess = nn.Sequential(
-                        self.normlayer,
-                )
+                self.normlayer,
+            )
 
         ## Input must be [0, 255], [3,244,244]
-        obs = obs.float() /  255.0
+        obs = obs.float() / 255.0
         obs_p = preprocess(obs)
         h = self.convnet(obs_p)
         return h
 
     def sim(self, tensor1, tensor2):
         if self.l2dist:
-            d = - torch.linalg.norm(tensor1 - tensor2, dim = -1)
+            d = -torch.linalg.norm(tensor1 - tensor2, dim=-1)
         else:
             d = self.cs(tensor1, tensor2)
         return d
 
 
+_vip_transforms = T.Compose(
+    [
+        T.Resize(256),
+        T.CenterCrop(224),
+        T.ToTensor(),  # this divides by 255
+        T.Normalize(
+            mean=[0.0, 0.0, 0.0], std=[1 / 255, 1 / 255, 1 / 255]
+        ),  # this will scale bact to [0-255]
+    ]
+)
 
-_vip_transforms = T.Compose([
-                        T.Resize(256),
-                        T.CenterCrop(224),
-                        T.ToTensor(),  # this divides by 255
-                        T.Normalize(mean=[0.0, 0.0, 0.0], std=[1/255, 1/255, 1/255]), # this will scale bact to [0-255]
-                    ])
 
 def load_model(home_dir, resnet_name="resnet50", metadata=None):
     model = load_vip(resnet_name, home_dir)
